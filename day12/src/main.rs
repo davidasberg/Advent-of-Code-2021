716f626cb30d2af8dc30b96f8d7cb1876597e77f
David Åsberg <dasberg@kth.se>
2021-12-13 13:57:31 +0100

    Did part 2 of day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -4,7 +4,32 @@ use std::fs;
 
 
 fn main() {
-    part_one();
+    //part_one();
+    part_two();
+}
+
+fn part_two(){
+    //read input from file 
+    let input = fs::read_to_string("input.txt").expect("Error reading file");
+    let edges = input.trim().split("\n").collect::<Vec<&str>>();
+    let mut graph = Graph::new();
+    for edge in edges {
+        let edge = edge.split("-").collect::<Vec<&str>>();
+        let node1 = edge[0].to_string();
+        let node2 = edge[1].to_string();
+        let is_small1 = node1 == node1.to_lowercase();
+        let is_small2 = node2 == node2.to_lowercase();
+        if node1 == "start" || node2 == "end" {
+            graph.add_edge(Node::new(node1,is_small1), Node::new(node2,is_small2));
+        }
+        else if node1 == "end" || node2 == "start" {
+            graph.add_edge(Node::new(node2,is_small1), Node::new(node1,is_small2));
+        }
+        else {
+            graph.add_double_edge(Node::new(node1,is_small1), Node::new(node2,is_small2));
+        }    }
+    let paths = graph.get_paths(graph.get_node("start").unwrap().clone(), graph.get_node("end").unwrap().clone(), 2);
+    println!("{}", paths.len());
 }
 
 fn part_one() {
@@ -18,9 +43,17 @@ fn part_one() {
         let node2 = edge[1].to_string();
         let is_small1 = node1 == node1.to_lowercase();
         let is_small2 = node2 == node2.to_lowercase();
-        graph.add_double_edge(Node::new(node1,is_small1), Node::new(node2,is_small2));
+        if node1 == "start" || node2 == "end" {
+            graph.add_edge(Node::new(node1,is_small1), Node::new(node2,is_small2));
+        }
+        else if node1 == "end" || node2 == "start" {
+            graph.add_edge(Node::new(node2,is_small1), Node::new(node1,is_small2));
+        }
+        else {
+            graph.add_double_edge(Node::new(node1,is_small1), Node::new(node2,is_small2));
+        }
     }
-    let paths = graph.get_paths(graph.get_node("start").unwrap().clone(), graph.get_node("end").unwrap().clone());
+    let paths = graph.get_paths(graph.get_node("start").unwrap().clone(), graph.get_node("end").unwrap().clone(), 1);
     println!("{}", paths.len());
 }
 
@@ -65,33 +98,34 @@ impl Graph {
    
     //search algorithm that finds all possible paths
     //can only visit small node once and large nodes can be visited multiple times
-    fn get_paths(&self, start: Node, end: Node) -> Vec<Vec<Node>> {
-        let mut paths: Vec<Vec<Node>> = Vec::new();
-        let mut visited: Vec<HashMap<Node, bool>> = Vec::new();
-        let mut stack = Vec::new();
+    fn get_paths(&self, start: Node, end: Node, small_max_visitations: u32) -> Vec<Vec<Node>> {
+   
+        let mut stack: Vec<(Vec<Node>, HashMap<Node, bool>, i32)> = Vec::new();
         let mut complete_paths: Vec<Vec<Node>> = Vec::new();
 
-        paths.push(vec![start.clone()]);
-        stack.push(start.clone());
-        let mut map = HashMap::new();
-        map.insert(start.clone(), true);
-        visited.push(map.clone());
+        let mut visited = HashMap::new();
+        visited.insert(start.clone(), true);
+
+        stack.push((vec![start.clone()], visited, 1));
+
         while !stack.is_empty() {
-            let node = stack.pop().unwrap();
-            let path = paths.pop().unwrap();
-            let visited_map = visited.pop().unwrap();
-            if node == end {
+            let (path, visited, small_visitations) = stack.pop().unwrap();
+            let node = path.last().unwrap();
+            if *node == end {
                 complete_paths.push(path);
             } else {
-                for neighbor in self.get_neighbors(&node) {
-                    if !neighbor.is_small || (neighbor.is_small && !visited_map.contains_key(&neighbor)) {
-                        let mut new_visited_map = visited_map.clone();
-                        new_visited_map.insert(neighbor.clone(), true);
-                        visited.push(new_visited_map);
-                        stack.push(neighbor.clone());
-                        let mut extended_path = path.clone();
-                        extended_path.push(neighbor.clone());
-                        paths.push(extended_path);
+                for neighbor in self.get_neighbors(node) {
+                    if !neighbor.is_small || !visited.contains_key(&neighbor) || small_visitations < small_max_visitations as i32 {
+                        let mut new_visited = visited.clone();
+                        new_visited.insert(neighbor.clone(), true);
+                        let mut new_path = path.clone();
+                        new_path.push(neighbor.clone());
+                        if neighbor.is_small && small_visitations < small_max_visitations as i32 && visited.contains_key(&neighbor){
+                            stack.push((new_path, new_visited, small_visitations + 1));
+                        }
+                        else {
+                            stack.push((new_path, new_visited, small_visitations));
+                        }
                     }
                 }
             }
@@ -118,7 +152,7 @@ impl Node {
     fn new(name: String, is_small: bool) -> Self {
         Node{
             name,
-            is_small
+            is_small,
         }
     }
 }
